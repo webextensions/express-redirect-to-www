004d9d27d0eebdbbfe0a5b3cf5c475e015d768e8
Priyank Parashar <paras20xx@gmail.com>
2020-03-23 06:11:09 +0400

    Improved error logging

--- index.js
+++ index.js
@@ -5,16 +5,35 @@ var redirectToWww = function (redirectStatusCode) {
         redirectStatusCode = 307; // Temporary redirect
     }
     return function (req, res, next) {
-        var host = req.get('host'),                 // req.get('host') contains port number as well
-            hostname = req.hostname;                // req.hostname doesn't contain port number
-        var wwwFound = hostname.match(/^www\./i),
-            dotFound = hostname.match(/\./),
-            isLikelyAnIpAddress = hostname.match(/^[\d]+\.[\d]+\.[\d]+\.[\d]+$/);   // A simple check (also satisfies number beyond IP address range, but it should be fine for normal use-cases)
-        if (!wwwFound && dotFound && !isLikelyAnIpAddress) {
-            var redirectToUrl = req.protocol + '://' + 'www.' + host + req.originalUrl;
-            return res.redirect(redirectStatusCode, redirectToUrl);
+        try {
+            var host = req.get('host'),                 // req.get('host') contains port number as well
+                hostname = req.hostname;                // req.hostname doesn't contain port number
+            var wwwFound = hostname.match(/^www\./i),
+                dotFound = hostname.match(/\./),
+                isLikelyAnIpAddress = hostname.match(/^[\d]+\.[\d]+\.[\d]+\.[\d]+$/);   // A simple check (also satisfies number beyond IP address range, but it should be fine for normal use-cases)
+            if (!wwwFound && dotFound && !isLikelyAnIpAddress) {
+                var redirectToUrl = req.protocol + '://' + 'www.' + host + req.originalUrl;
+                return res.redirect(redirectStatusCode, redirectToUrl);
+            }
+            next();
+        } catch (e) {
+            console.error(e);
+            console.error(
+                `Error: Error caught in express-redirect-to-https.` +
+                ` req.headers = ${req.headers} ;` +
+                ` req.host = ${req.host} ;` +
+                ` req.hostname = ${req.hostname} ;` +
+                ` req.method = ${req.method} ;` +
+                ` req.originalUrl = ${req.originalUrl} ;` +
+                ` req.path = ${req.path} ;` +
+                ` req.protocol = ${req.protocol} ;` +
+                ` req.subdomains = ${req.subdomains} ;` +
+                ` req.url = ${req.url} ;` +
+                ` req._parsedUrl = ${req._parsedUrl} ;`
+            );
+
+            throw e;
         }
-        next();
     };
 };
 module.exports = redirectToWww;
