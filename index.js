bc5f2d68afb0b071c907b75c4b2805d4a2f34e95
Priyank Parashar <paras20xx@gmail.com>
2020-03-24 04:29:54 +0400

    Improved robustness

--- index.js
+++ index.js
@@ -6,21 +6,29 @@ var redirectToWww = function (redirectStatusCode) {
     }
     return function (req, res, next) {
         try {
-            var host = req.get('host'),                 // req.get('host') contains port number as well
-                hostname = req.hostname;                // req.hostname doesn't contain port number
-            var wwwFound = hostname.match(/^www\./i),
-                dotFound = hostname.match(/\./),
-                isLikelyAnIpAddress = hostname.match(/^[\d]+\.[\d]+\.[\d]+\.[\d]+$/);   // A simple check (also satisfies number beyond IP address range, but it should be fine for normal use-cases)
-            if (!wwwFound && dotFound && !isLikelyAnIpAddress) {
-                var redirectToUrl = req.protocol + '://' + 'www.' + host + req.originalUrl;
-                return res.redirect(redirectStatusCode, redirectToUrl);
+            if (
+                typeof req.get('host') === 'string' &&
+                typeof req.hostname === 'string'
+            ) {
+                var host = req.get('host'),                 // req.get('host') contains port number as well
+                    hostname = req.hostname;                // req.hostname doesn't contain port number
+                var wwwFound = hostname.match(/^www\./i),
+                    dotFound = hostname.match(/\./),
+                    isLikelyAnIpAddress = hostname.match(/^[\d]+\.[\d]+\.[\d]+\.[\d]+$/);   // A simple check (also satisfies number beyond IP address range, but it should be fine for normal use-cases)
+                if (!wwwFound && dotFound && !isLikelyAnIpAddress) {
+                    var redirectToUrl = req.protocol + '://' + 'www.' + host + req.originalUrl;
+                    return res.redirect(redirectStatusCode, redirectToUrl);
+                }
+                next();
+            } else { // To reach this "else" condition, try something like: curl -H "host:" 127.0.0.1
+                return res.status(400).send('400 Bad Request - Please access the "www." version of this website.\n');
             }
-            next();
         } catch (e) {
             console.error(e);
             console.error(
                 `Error: Error caught in express-redirect-to-https.` +
                 ` req.headers = ${JSON.stringify(req.headers)} ;` +
+                ` req.get('host') = ${req.get('host')} ;` +
                 ` req.host = ${req.host} ;` +
                 ` req.hostname = ${req.hostname} ;` +
                 ` req.method = ${req.method} ;` +
